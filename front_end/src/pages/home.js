1da0e6e84b37057f3e12130cbc2d67244bd44816
kamilamin95 <muhammad.kamil@logicielservice.com>
2023-09-21 13:21:20 +0500

    Design home page login and signup ui

--- front_end/src/pages/home.js
+++ front_end/src/pages/home.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import * as React from 'react';
+import Header from '../components/header';
 
-function Home() {
+export default function Home() {
   return (
-    <div>home</div>
-  )
+    <Header />
+  );
 }
-
-export default Home
