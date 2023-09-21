b590af6afb76cc24df67413d48f966cea637fde8
kamilamin95 <muhammad.kamil@logicielservice.com>
2023-09-21 17:28:38 +0500

    Update used schema and integrate API routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const mongoose = require("mongoose")
+const router = require('./routes/user-routes')
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const port = 5000;
+require('dotenv').config()
+
+const app = express();
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api', router)
 
 
-app.get('/', (req, res) => {
-    res.send("Hello World!!")
-})
-
-app.listen(port, () => {
-    console.log(`App is running on ${port}`);
-})
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.ejlha13.mongodb.net/mern-auth?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(5000);
+    console.log('Database Connected! App is listening at 5000 port')
+}).catch((error) => console.log(error))
---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        require: true
+    },
+    lastName: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
