441447f8280170021fc17b767083225505b68e79
kamilamin95 <muhammad.kamil@logicielservice.com>
2023-09-21 12:18:14 +0500

    Implement routing and connect pages

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 function App() {
   return (
-    <div className="App">
-      Logiciel Chat App
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
