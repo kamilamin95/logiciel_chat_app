3608b70aec487fedb50aa164c6bb08c17d714cf3
kamilamin95 <muhammad.kamil@logicielservice.com>
2023-09-21 15:54:51 +0500

    Implement onchange methods and validations on login and signup forms

--- front_end/src/pages/login.js
+++ front_end/src/pages/login.js
@@ -1,28 +1,50 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const defaultTheme = createTheme();
 
 export default function Login() {
-  
-  
+  const [emailError, setEmailError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleChange = (event) => {
+    let name = event.target.name;
+    if (name === "email") {
+      setEmailError(false)
+      setEmail(event.target.value);
+    }
+
+    if (name === "password") {
+      setPasswordError(false)
+      setPassword(event.target.value);
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
+    let email = data.get("email");
+    let password = data.get("password");
+    if (email === "" && password === "") {
+      setEmailError(true);
+      setPasswordError(true);
+    }
   };
 
   return (
@@ -32,19 +54,25 @@ export default function Login() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
+              error={emailError}
               margin="normal"
               required
               fullWidth
@@ -53,8 +81,11 @@ export default function Login() {
               name="email"
               autoComplete="email"
               autoFocus
+              value={email}
+              onChange={(event) => handleChange(event)}
             />
             <TextField
+              error={passwordError}
               margin="normal"
               required
               fullWidth
@@ -63,6 +94,8 @@ export default function Login() {
               type="password"
               id="password"
               autoComplete="current-password"
+              value={password}
+              onChange={(event) => handleChange(event)}
             />
             {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
--- front_end/src/pages/signup.js
+++ front_end/src/pages/signup.js
@@ -1,27 +1,76 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 const defaultTheme = createTheme();
 
 export default function Signup() {
+  const [firstNameError, setFirstNameError] = React.useState(false);
+  const [lastNameError, setLastNameError] = React.useState(false);
+  const [emailError, setEmailError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
+
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleChange = (event) => {
+    let name = event.target.name;
+
+    if (name === "firstName") {
+      setFirstNameError(false);
+      setFirstName(event.target.value);
+    }
+
+    if (name === "lastName") {
+      setLastNameError(false);
+      setLastName(event.target.value);
+    }
+
+    if (name === "email") {
+      setEmailError(false);
+      setEmail(event.target.value);
+    }
+
+    if (name === "password") {
+      setPasswordError(false);
+      setPassword(event.target.value);
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
+    let firstName = data.get("firstName");
+    let lastName = data.get("lastName");
+    let email = data.get("email");
+    let password = data.get("password");
+    if (
+      firstName === "" &&
+      lastName === "" &&
+      email === "" &&
+      password === ""
+    ) {
+      setFirstNameError(true);
+      setLastNameError(true);
+      setEmailError(true);
+      setPasswordError(true);
+    }
   };
 
   return (
@@ -31,21 +80,27 @@ export default function Signup() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
+                  error={firstNameError}
                   autoComplete="given-name"
                   name="firstName"
                   required
@@ -53,30 +108,39 @@ export default function Signup() {
                   id="firstName"
                   label="First Name"
                   autoFocus
+                  value={firstName}
+                  onChange={(event) => handleChange(event)}
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
+                  error={lastNameError}
                   required
                   fullWidth
                   id="lastName"
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
+                  value={lastName}
+                  onChange={(event) => handleChange(event)}
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
+                  error={emailError}
                   required
                   fullWidth
                   id="email"
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  value={email}
+                  onChange={(event) => handleChange(event)}
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
+                  error={passwordError}
                   required
                   fullWidth
                   name="password"
@@ -84,6 +148,8 @@ export default function Signup() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  value={password}
+                  onChange={(event) => handleChange(event)}
                 />
               </Grid>
               {/* <Grid item xs={12}>
