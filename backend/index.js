10a5c77e96197f8a0e859603166030b49a76283f
kamilamin95 <muhammad.kamil@logicielservice.com>
2023-09-22 15:24:25 +0500

    implement socket io connection on backend

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,32 @@
 const express = require("express");
-const mongoose = require("mongoose")
-const router = require('./routes/user-routes')
+const app = express();
+const mongoose = require("mongoose");
+const router = require("./routes/user-routes");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const http = require('http')
+const server = http.createServer(app)
+const {Server} = require("socket.io");
+const io = new Server(server)
 const port = 5000;
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express();
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
-app.use('/api', router)
+app.use("/api", router);
 
+io.on('connection', (socket) => {
+    console.log('New client connection', socket);
+    socket.emit('connection', null);
+})
 
-mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.ejlha13.mongodb.net/mern-auth?retryWrites=true&w=majority`)
-.then(() => {
-    app.listen(5000);
-    console.log('Database Connected! App is listening at 5000 port')
-}).catch((error) => console.log(error))
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.ejlha13.mongodb.net/mern-auth?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    server.listen(5000);
+    console.log("Database Connected! App is listening at 5000 port");
+  })
+  .catch((error) => console.log(error));
