dec228ac573311166a27b01fb7321d9da45f5caf
kamilamin95 <muhammad.kamil@logicielservice.com>
2023-09-22 16:10:41 +0500

    Developed socket connection on frontend

--- front_end/src/pages/chatRoom.js
+++ front_end/src/pages/chatRoom.js
@@ -1,7 +1,19 @@
 import React from "react";
 import Header from "../components/header";
+// import {io} from 'socket.io-client'
+import socketClient from 'socket.io-client'
+
 
 function ChatRoom() {
+  var socket = socketClient('http://localhost:5000')
+
+  socket.on('connection', () => {
+    console.log('Socket connection is established');
+  })
+
+
+  // console.log('SOCKET', socket);
+
   return (
     <>
       <Header />
