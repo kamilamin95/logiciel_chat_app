ed270c9ef8d8050d1c5f9cb6a2a16fca7ec15b63
kamilamin95 <muhammad.kamil@logicielservice.com>
2023-09-25 20:26:06 +0500

    Integrate chat components

--- front_end/src/pages/chatPage.js
+++ front_end/src/pages/chatPage.js
@@ -3,6 +3,9 @@ import Header from "../components/header";
 import "../styles/chatRoom.css";
 import { Button, TextField } from "@mui/material";
 import { getUserDetails } from "../services/auth";
+import ChatBar from "../components/ChatBar";
+import ChatBody from '../components/ChatBody'
+import ChatFooter from "../components/ChatFooter";
 
 function ChatPage({ socket }) {
   const [textMessage, setTextMessage] = useState("");
@@ -10,31 +13,40 @@ function ChatPage({ socket }) {
 
   const sendMessage = () => {
     socket.emit("message", textMessage);
-    setTextMessage('')
+    setTextMessage("");
   };
 
   const fetchUserDetails = async () => {
-    await getUserDetails().then(response => {
-      sessionStorage.setItem('firstName', response.data.user.firstName)
-      sessionStorage.setItem('lastName', response.data.user.lastName)
-      sessionStorage.setItem('email', response.data.user.email)
-    }).catch(error => console.log(error))
-  }
+    await getUserDetails()
+      .then((response) => {
+        sessionStorage.setItem("firstName", response.data.user.firstName);
+        sessionStorage.setItem("lastName", response.data.user.lastName);
+        sessionStorage.setItem("email", response.data.user.email);
+      })
+      .catch((error) => console.log(error));
+  };
 
   useEffect(() => {
     socket.on("message", function (msg) {
       setServerMessages([...serverMessages, msg]);
     });
   }, [socket, serverMessages]);
-  
+
   useEffect(() => {
-    fetchUserDetails()
-  }, [])
+    fetchUserDetails();
+  }, []);
 
   return (
     <>
       <Header />
-      <TextField
+      <div className="chat">
+        <ChatBar />
+        <div className="chat__main">
+          <ChatBody />
+          <ChatFooter socket={socket} />
+        </div>
+      </div>
+      {/* <TextField
         type="text"
         value={textMessage}
         onChange={(ev) => setTextMessage(ev.target.value)}
@@ -45,7 +57,7 @@ function ChatPage({ socket }) {
 
       {serverMessages.map((item, i) => {
         return <li key={i}>{item}</li>;
-      })}
+      })} */}
     </>
   );
 }
